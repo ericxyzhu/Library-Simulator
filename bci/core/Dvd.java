4dd25b0e71c5fbc4454a1412bd44fc3ac02f82a8
Gabriel <gabrielmmonte06@gmail.com>
2025-10-02 16:06:23 +0100

    Forgot things from previous

--- bci/core/Dvd.java
+++ bci/core/Dvd.java
@@ -9,8 +9,15 @@ public class Dvd extends Obra {
     public Dvd (String title, int price, Categoria categoria, int copies, String igac, Criador realizador){
         super(title, price, categoria, copies);
         _realizador = realizador;
+        _igac = igac;
 
     }
+
+    public String toString(){
+        return "Dvd -" + super.gettitle() + " - " + super.getprice() + " - " + super.getcategoria() + " - " + "Criador.toString() _realizador" + " - " + _igac;
+
+    }
+
 
 
 

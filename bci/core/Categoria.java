58197f97d0ae2b4d119f2fa8e95e58320827a858
Gabriel <gabrielmmonte06@gmail.com>
2025-10-05 16:34:29 +0100

    jata

--- bci/core/Categoria.java
+++ bci/core/Categoria.java
@@ -1,7 +1,7 @@
 package bci.core;
 
 public enum Categoria {
-    REFERENCE("Referência"), FICTION("Ficção"), SCITECH("Técnica e Ciêntífica");
+    REFERENCE("Referência"), FICTION("Ficção"), SCITECH("Técnica e Científica");
     private String _categoria;
     private Categoria (String categoria){
         _categoria = categoria;
